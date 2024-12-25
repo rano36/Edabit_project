292d2f1e26f86e41e447e70b010ec4725862d66e
Rano <ranotoktorbaeva@gmail.com>
2024-12-26 00:52:15 +0600

    Edabit_Middle

--- src/test/java/edabit_project/Edabit_Middle.java
+++ src/test/java/edabit_project/Edabit_Middle.java
@@ -19,20 +19,20 @@ public class Edabit_Middle {
         @Test
         public void testOtherSides() {
             List<Double> result = Edabit_Middle.otherSides(1);
-            Assert.assertEquals(2.0, result.get(0)); // Hypotenuse = 2.0
-            Assert.assertEquals(1.73, result.get(1)); // Other side = 1.73
+            Assert.assertEquals(2.0, result.get(0));
+            Assert.assertEquals(1.73, result.get(1));
 
             result = Edabit_Middle.otherSides(12);
-            Assert.assertEquals(24.0, result.get(0)); // Hypotenuse = 24.0
-            Assert.assertEquals(20.78, result.get(1)); // Other side = 20.0
+            Assert.assertEquals(24.0, result.get(0));
+            Assert.assertEquals(20.78, result.get(1));
 
             result = Edabit_Middle.otherSides(2);
-            Assert.assertEquals(4.0, result.get(0));  // Hypotenuse = 4.0
-            Assert.assertEquals(3.46, result.get(1)); // Other side = 3.46
+            Assert.assertEquals(4.0, result.get(0));
+            Assert.assertEquals(3.46, result.get(1));
 
             result = Edabit_Middle.otherSides(3);
-            Assert.assertEquals(6.0, result.get(0));  // Hypotenuse = 6.0
-            Assert.assertEquals(5.2, result.get(1));  // Other side = 5.2
+            Assert.assertEquals(6.0, result.get(0));
+            Assert.assertEquals(5.2, result.get(1));
         }
     }
 
